eff4ef9b3bbf5e46b003e5e033076f411a09eaab
Noopy94 <122508464+Noopy94@users.noreply.github.com>
2023-03-16 21:00:54 +0900

    Update PS7576_DuHyun.java

--- assignment/DFS_and_BFS/PS7576_DuHyun.java
+++ assignment/DFS_and_BFS/PS7576_DuHyun.java
@@ -1,5 +1,3 @@
-package baekjoon;
-
 import java.awt.Point;
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -10,78 +8,77 @@ import java.util.StringTokenizer;
 
 public class Main7576 {
 
-	static int M;
+	static int[][] arr;
 	static int N;
-	static int H;
-	static int[][][] tomato;
-	static boolean[][][] visited;
-	static int day;
-	static int result;
-	static Queue<pointer> Q;
+	static int M;
+	static int cnt;
+	static Queue<Point> Q;
+	static int[] dx = { 0, 0, 1, -1 };
+	static int[] dy = { 1, -1, 0, 0 };
+	static int day = 0;
 
 	public static void main(String[] args) throws IOException {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		M = Integer.parseInt(st.nextToken()); // 세로(가로칸의 수)
-		N = Integer.parseInt(st.nextToken()); // 가로(세로칸의 수)
-		H = Integer.parseInt(st.nextToken()); // 상자의 수
-		tomato = new int[N][M][H];
-		visited = new boolean[N][M][H];
-		day = -1;
-		int cnt = 0; // 토마토 총 갯수
-		result = 0;
-		Q = new LinkedList<pointer>();
+		M = Integer.parseInt(st.nextToken());
+		N = Integer.parseInt(st.nextToken());
+		arr = new int[N][M];
+		cnt = 0;
+		int cnt2 = 0;
+		Q = new LinkedList<Point>();
 
-		for (int tc = 0; tc < H; tc++) {
-
-			for (int i = 0; i < N; i++) {
-				st = new StringTokenizer(br.readLine());
-				for (int j = 0; j < M; j++) {
-					tomato[i][j][tc] = Integer.parseInt(st.nextToken());
-					if (tomato[i][j][tc] == 0 ) cnt++;
-					if (tomato[i][j][tc] == 1) {
-						cnt++;
-						Q.offer(new pointer(i,j,tc));
-					}
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < M; j++) {
+				arr[i][j] = Integer.parseInt(st.nextToken());
+				if (arr[i][j] == 1) {
+					cnt++;
+					Q.offer(new Point(i, j));
+				} else if (arr[i][j] == -1) {
+					cnt2++;
 				}
 			}
-
 		}
 
-		bfs();
-
-		if (result == cnt)
-			System.out.println(day);
-		else
+		if (cnt == 0)
 			System.out.println(-1);
-
-	}
-	
-	static void bfs() {
-		
-		while (!Q.isEmpty()) {
-			
-			
-			
-			
+		else if (cnt == N * M - cnt2) {
+			System.out.println(0);
+		} else {
+			bfs();
+			if (cnt == N * M - cnt2) {
+				System.out.println(day-1);
+			} else
+				System.out.println(-1);
 		}
-		
-		
-		
+
 	}
 
-}
+	static void bfs() {
 
-class pointer {
-	int x;
-	int y;
-	int z;
+		while (!Q.isEmpty()) {
 
-	pointer(int x, int y, int z) {
-		this.x = x;
-		this.y = y;
-		this.z = z;
+			int A = Q.size();
+			day++;
+
+			for (int j = 0; j < A; j++) {
+				Point p = Q.poll();
+
+				for (int i = 0; i < 4; i++) {
+					int x1 = p.x + dx[i];
+					int y1 = p.y + dy[i];
+
+					if (0 <= x1 && x1 < N && 0 <= y1 && y1 < M && arr[x1][y1] == 0) {
+						Q.offer(new Point(x1, y1));
+						arr[x1][y1] = 1;
+						cnt++;
+					}
+
+				}
+			}
+		}
+
 	}
 
 }
